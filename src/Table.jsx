7cec9c1c2fab49ae97e86bc0f0e01e5d4d3d9acf
ninemilli-song <sxg19851005@sina.com>
2016-08-25 14:49:27 +0800

    if maxHeight greater than the minHeight, then use the maxHeight

--- src/Table.jsx
+++ src/Table.jsx
@@ -398,6 +398,10 @@ const Table = React.createClass({
       if (bodyStyle.minHeight) {
         bodyStyle.minHeight = bodyStyle.minHeight || scroll.y;
       }
+      // if maxHeight greater than the minHeight, then use the maxHeight. by songxg 2016/08/25
+      if (body.maxHeight > body.minHeight) {
+        body.minHeight = body.maxHeight;
+      }
       bodyStyle.overflowY = bodyStyle.overflowY || 'scroll';
       useFixedHeader = true;
 
